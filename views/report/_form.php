1e2f403777964c377d489698a55bb31a87f1e51c
metaaa <baloghmarton.geo@gmail.com>
2019-06-26 09:02:38 +0200

    readonly form field added

--- views/report/_form.php
+++ views/report/_form.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+$model->reporterId = Yii::$app->getUser()->id;
+
 /* @var $this yii\web\View */
 /* @var $model app\models\Report */
 /* @var $form yii\widgets\ActiveForm */
@@ -12,11 +14,11 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'room')->textInput() ?>
+    <?= $form->field($model, 'room')->dropDownList(array('1' => '1', '2' => '2', '3' => '3')) ?>
 
-    <?= $form->field($model, 'status')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'status')->dropDownList(array('down' => 'down', 'clear' => 'clear'))?>
 
-    <?= $form->field($model, 'reporterId')->textInput() ?>
+    <?= $form->field($model, 'reporterId')->textInput(['readonly' => true]) ?>
 
     <?= $form->field($model, 'suspectId')->textInput() ?>
 
